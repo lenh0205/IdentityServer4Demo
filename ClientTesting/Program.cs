91a8dc2268f02625104c7f53e63fd593feafa9ce
vietinfo123 <lenhtranchi98@gmail.com>
2024-08-28 17:53:59 +0700

    firt commit

---
+++ ClientTesting/Program.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel.Client;
+using static IdentityModel.OidcConstants;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+// request token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
